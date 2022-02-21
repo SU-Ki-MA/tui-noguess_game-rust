c72430fd4edddbf127754306e2ba1e4876967e54
SU-Ki-MA <jsivabalan2002@gmail.com>
2022-02-21 20:16:06 +0530

    initial release

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
     //secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Secret number is: {}", secret_number);
+    let mut guess = String::new();
     loop {
-        println!("Please input your guess.");
-        let mut guess = String::new();
+        println!("Please input your guess :");
+        String::clear(&mut guess);
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
